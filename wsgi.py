0f36599419fa7d9943cd06da58d3875ce0e07da7
Koce Georgiev <koce@mentalchallenge.tk>
2020-01-19 23:55:02 +0100

    Hello server timestamp

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from time import strftime
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello There!"
+    return "Hello,{}".format(strftime('%Y-%m-%d %H:%M:%S'))
 
 if __name__ == "__main__":
     application.run()
