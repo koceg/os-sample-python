a966a9b8f4a5ac34057df4c30548b867655c92dd
Koce Georgiev <koce@mentalchallenge.tk>
2020-02-03 22:01:57 +0100

    small update

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello,{}".format(strftime('%Y-%m-%d %H:%M:%S'))
+    return "Hello from s2i-git ,{}".format(strftime('%Y-%m-%d %H:%M:%S'))
 
 if __name__ == "__main__":
     application.run()
