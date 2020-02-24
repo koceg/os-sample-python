6cecd337a6baa4785e93d310ca559c1a941ccd34
root <root@ip-10-0-0-180.eu-central-1.compute.internal>
2020-02-24 21:05:41 +0000

    Source update

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Running s2i image ,{}".format(strftime('%Y-%m-%d %H:%M:%S'))
+    return "20200224--Running s2i image ,{}".format(strftime('%Y-%m-%d %H:%M:%S'))
 
 if __name__ == "__main__":
     application.run()
